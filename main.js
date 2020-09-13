1d52620b998bc200cc0701619f1aa56e65fbe0d6
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2020-09-14 01:38:09 +0900

    JS: add transition

--- main.js
+++ main.js
@@ -4,17 +4,20 @@ const startbtn = document.querySelector('.lotto_start');
 const resetbtn = document.querySelector('.lotto_reset');
 const lottolist = document.querySelector('.lotto');
 
+// Start 버튼 클릭 시
 startbtn.addEventListener('click', () => {
   resetbtn.classList.remove('invisible');
   startbtn.classList.add('invisible');
   paintlotto();
 });
 
+// 랜덤한 값 리턴함수
 function getrandom() {
   const number = Math.floor(Math.random() * 50 + 1);
   return number;
 }
 
+// 로또 번호 생성
 function paintlotto() {
   for (let i = 1; i < 8; i++) {
     let li = document.createElement('li');
@@ -26,6 +29,7 @@ function paintlotto() {
   }
 }
 
+// Reset 버튼 클릭 시
 resetbtn.addEventListener('click', () => {
   for (let i = 1; i < 8; i++) {
     let numberli = document.querySelector(`.lotto__number_${i}`);
